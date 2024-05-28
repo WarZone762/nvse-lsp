95eca392e97f46039f5d0665f79a89a30bddbbeb
WarZone762 <uzi1m1@gmail.com>
2024-05-28 06:30:22 +0400

    feat(vscode): add VSCode extonsion

---
+++ vscode/src/extension.ts
@@ -0,0 +1,42 @@
+import { ExtensionContext, Uri } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const serverModule = Uri.joinPath(
+    context.extensionUri,
+    "dist",
+    "nvse-lsp",
+  ).fsPath;
+
+  const serverOptions: ServerOptions = {
+    run: { command: serverModule },
+    debug: { command: serverModule },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "geckscript-nvse" }],
+  };
+
+  client = new LanguageClient(
+    "nvse-lsp",
+    "NVSE Language Server",
+    serverOptions,
+    clientOptions,
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
